b47fb38e8bd84d63398aa77d18bec75ba38036a1
Junpei Kawamoto <junpei@d16-150.csce.kyushu-u.ac.jp>
2015-07-27 20:39:03 +0900

    Add fabfile.

---
+++ fabfile.py
@@ -0,0 +1,21 @@
+from fabric.api import *
+from fabric.contrib import files
+env.use_ssh_config = True
+
+PACKAGE = "google-fluentd"
+
+@task
+def deploy():
+    """ Upload contents. """
+    if not files.exists(PACKAGE):
+        run("mkdir " + PACKAGE)
+    with cd(PACKAGE):
+        put("Dockerfile", ".")
+        put("conf", ".", mirror_local_mode=True)
+
+
+@task
+def build():
+    """ Build a docker image. """
+    with cd(PACKAGE):
+        run("docker build -t {tag} .".format(tag=PACKAGE))
